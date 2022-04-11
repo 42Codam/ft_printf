71c03d2eda08e49f8574cfa3155ab602a8d205b5
Ramazan Bulbul <29.rbulbul@gmail.com>
2022-04-11 19:44:15 +0200

    Parse updated

--- srcs/ft_parse_type.c
+++ srcs/ft_parse_type.c
@@ -6,24 +6,29 @@
 /*   By: rbulbul <rbulbul@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/03/21 19:39:31 by rbulbul       #+#    #+#                 */
-/*   Updated: 2022/04/08 14:32:58 by rbulbul       ########   odam.nl         */
+/*   Updated: 2022/04/11 18:59:42 by rbulbul       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-int	ft_parse_type(t_flags *tab, const char type)
+void	ft_parse_type(t_flags *tab, const char type)
 {
-
 	if (type == 'c')
 		ft_print_ch(tab);
 	else if (type == 's')
 		ft_print_str(tab);
 	else if (type == 'i' || type == 'd')
 		ft_print_int(tab);
+	else if (type == 'u')
+		ft_print_unint(tab);
 	else if (type == 'x' || type == 'X')
 		ft_print_hex(type, tab);
+	else if (type == 'p')
+		ft_print_ptr(tab);
 	else if (type == '%')
-		write(1, "%%", 1);
-	return (0);
+	{
+		tab->total_length += 1;
+		write(1, &type, 1);
+	}
 }
